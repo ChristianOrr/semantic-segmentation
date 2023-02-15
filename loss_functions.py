7cd0c0d288c9cfddea4c369cff8e8174310af3d1
ChristianOrr <orrchristian@gmail.com>
2023-02-15 22:07:56 +0200

    added mse loss

--- loss_functions.py
+++ loss_functions.py
@@ -51,6 +51,27 @@ def dice_loss_and_preds(variables, state, inputs, targets, epsilon):
 
 
 @jax.jit
+def mse_loss(variables, state, inputs, targets):
+    """
+    Calculates the mean squared error loss for a batch of images.
+    Args:
+        variables: The segmentation models parameters.
+        state: State of the semantic segmentation model.
+        inputs: A batch of raw input images.
+        targets: A batch of arrays with the same shape as inputs. Stores the binary
+                 classification label for each element in inputs
+                (0 for the negative class and 1 for the positive class).
+    Returns:
+        The dice loss from a batch of images.
+    """
+    forward_fn = state.apply_fn
+    preds = forward_fn(variables, inputs)
+    loss = jnp.mean((preds - targets)**2)
+    return loss
+
+
+
+@jax.jit
 def dice_loss_bn(variables, state, inputs, targets, epsilon):
     """
     Calculates the dice loss for a batch of images.
