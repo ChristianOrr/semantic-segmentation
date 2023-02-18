d9b4076c58165c50faac21830ac5b960547fb10f
ChristianOrr <orrchristian@gmail.com>
2023-02-18 14:29:20 +0200

    updated unet

--- unet_train.py
+++ unet_train.py
@@ -1,7 +1,7 @@
 import os
+import json
 import numpy as np
 import jax
-from jax import value_and_grad
 import jax.numpy as jnp
 from flax.core.frozen_dict import freeze, unfreeze
 import optax
@@ -10,17 +10,19 @@ from flax.training import train_state, checkpoints
 import augmax
 import orbax.checkpoint as orbax
 from unet_model import _unet
-from loss_functions import dice_loss, dice_loss_and_preds
-from preprocessing_functions import prep_data_batch, create_infinite_generator, grads_vanished_or_exploded
+from loss_functions import loss_fn
+from preprocessing_functions import prep_data_batch, create_infinite_generator, grads_vanished_or_exploded, dict_mean
+from miou_metrics import compute_metrics
 import nest_asyncio
 nest_asyncio.apply()
 import argparse
-
+from jax.config import config
 
 
 parser = argparse.ArgumentParser(description='Script for training U-Net')
 parser.add_argument("--dataset_name", help='Dataset name to load from huggingface hub', default="scene_parse_150", required=False)
-parser.add_argument("--num_classes", help="Number of classes in the dataset", default=151, type=int, required=False)
+parser.add_argument("--num_classes", help="Number of classes in the dataset", default=150, type=int, required=False)
+parser.add_argument("--ignore_label", help="Label to ignore for training", default=0, type=int, required=False)
 parser.add_argument("--weights_path", help='Path to save and load checkpoints', default="./checkpoints/unet/", required=False)
 parser.add_argument("--init_function", 
                     help='Function for initializing the weights (not needed if restoring weights)' 
@@ -29,14 +31,15 @@ parser.add_argument("--init_function",
                     required=False)
 parser.add_argument("--rng", help="Random number generator key", default=64, type=int, required=False)
 parser.add_argument("--print_freq", help="Frequency for displaying training loss", default=1, type=int, required=False)
+parser.add_argument("--loss_method", help='Loss calculation method. Options include: "dice" and "cross_entropy".', default="dice", required=False)
 parser.add_argument("--lr", help="Initial value for learning rate.", default=0.00001, type=float, required=False)
 parser.add_argument("--min_lr", help="Minimum learning rate cap.", default=0.000001, type=float, required=False)
 parser.add_argument("--decay", help="Exponential decay rate.", default=0.99, type=float, required=False)
-parser.add_argument("--decay_start", help="The epoch to start decaying the learning rate.", default=100, type=int, required=False)
-parser.add_argument("--decay_steps", help="Transition steps before next lr decay.", default=100, type=int, required=False)
+parser.add_argument("--decay_start", help="The epoch to start decaying the learning rate.", default=200, type=int, required=False)
+parser.add_argument("--decay_steps", help="Transition steps before next lr decay.", default=10, type=int, required=False)
 parser.add_argument("--height", help='Model image input height resolution', type=int, default=256)
 parser.add_argument("--width", help='Model image input height resolution', type=int, default=256)
-parser.add_argument("--batch_size", help='Batch size to use during training',type=int, default=24)
+parser.add_argument("--batch_size", help='Batch size to use during training',type=int, default=1)
 parser.add_argument("--float_precision", help='Floating point precision, Options are 32 and 16.',type=int, default=32)
 parser.add_argument("--num_epochs", help='Number of training epochs', type=int, default=100000)
 parser.add_argument("--save_freq", help='Model saving frequncy per steps', type=int, default=100)
@@ -44,11 +47,14 @@ parser.add_argument("--val_epochs", help='Frequency for running evaluation', typ
 parser.add_argument("--val_batches", help='Number of batches to process per evaluation', type=int, default=5)
 parser.add_argument("--dont_augment", help="Prevents augmentation on the RGB images.", action="store_true")
 parser.add_argument("--dont_restore", help="Prevents restoring the latest checkpoint from the weights_path", action="store_true")
+parser.add_argument("--debugging", help="Runs in debuggin mode.", action="store_true")
 args = parser.parse_args()
 
 
 
 def main(args):
+    if args.debugging:
+        config.update("jax_disable_jit", True)
 
     dataset = load_dataset(args.dataset_name)
     train_dataset, val_dataset  = dataset["train"], dataset["validation"]
@@ -93,8 +99,8 @@ def main(args):
     # Checkpointing frequency
     save_steps = args.save_freq
     batch_size = args.batch_size
-    # A small number to prevent loss function from dividing by zero
-    epsilon = 0.0001
+    # Background class
+    ignore_label = args.ignore_label
     # Exponential decay learning rate schedule defined by:
     # learning_rate * decay_rate ^ (global_step / decay_steps)
     schedule = optax.exponential_decay(
@@ -128,49 +134,51 @@ def main(args):
     val_data_generator = create_infinite_generator(val_dataset)
 
     losses = []
-    val_losses = []
 
     for epoch in range(epochs + 1):
         if epoch % print_freq == 0: print(f"Epoch: {epoch}")
         # Evaluate the models performance
         if epoch % val_epochs == 0:
-            val_batch_losses = []
+            eval_batch_metrics = []
             for _ in range(val_batches):
-                # Prepare a batch of data
                 val_inputs = []
                 # Loop until a valid batch is found
                 while len(val_inputs) == 0:
-                    val_inputs, val_targets = prep_data_batch(val_data_generator, batch_size, height, width, num_classes, dtype=jnp.float32)
+                    val_inputs, val_targets = prep_data_batch(val_data_generator, batch_size, height, width, dtype=jnp.float32)
                 # Perform augmentation
                 rng_key, subkey = jax.random.split(rng_key)
                 if augment: val_inputs = augment_images(subkey, val_inputs)
                 # Get loss and preds
                 variables = freeze({"params": state.params})
-                val_batch_loss, val_batch_preds = dice_loss_and_preds(variables, state, val_inputs, val_targets, epsilon)
-                val_batch_losses.append(val_batch_loss)
-            val_loss = jnp.array(val_batch_losses).mean()
-            val_losses.append(val_loss)
-            print(f"\t\tValidation Loss: {val_loss :.2f}")
+                val_loss, val_logits = loss_fn(variables, state, val_inputs, val_targets, num_classes, args.loss_method, ignore_label)
 
-        # Prepare a batch of data
+                eval_metrics = compute_metrics(val_logits, val_targets, num_classes, ignore_label)
+                eval_metrics["loss"] = val_loss
+                eval_batch_metrics.append(eval_metrics)
+
+            eval_batch_metrics = dict_mean(eval_batch_metrics)
+            print("Validation Metrics:")
+            print(json.dumps(eval_batch_metrics, indent=4))
+
         inputs = []
         # Loop until a valid batch is found
         while len(inputs) == 0:
-            inputs, targets = prep_data_batch(train_data_generator, batch_size, height, width, num_classes, dtype=jnp.float32)
+            inputs, targets = prep_data_batch(train_data_generator, batch_size, height, width, dtype=jnp.float32)
         # Perform augmentation
         rng_key, subkey = jax.random.split(rng_key)
         if augment: inputs = augment_images(subkey, inputs)
         # Perform backpropagation 
         variables = freeze({"params": state.params})
-        loss, grads = value_and_grad(dice_loss, argnums=0)(variables, state, inputs, targets, epsilon)
+        grad_fn = jax.value_and_grad(loss_fn, argnums=0, has_aux=True)
+        (loss, logits), grads = grad_fn(variables, state, inputs, targets, num_classes, args.loss_method, ignore_label)
         state = state.apply_gradients(grads=grads["params"])
 
         losses.append(loss)
         last_mean_loss = np.array(losses[-print_freq:]).mean()
-        if epoch % print_freq == 0: print(f"\tLoss: {last_mean_loss :.2f} \tLearning Rate: {schedule(epoch) :.8f}")
+        if epoch % print_freq == 0: print(f"\tLoss: {last_mean_loss :.4f} \tLearning Rate: {schedule(epoch) :.8f}")
 
         has_vanished,  has_exploded, mean_grads = grads_vanished_or_exploded(
-            unfreeze(grads["params"]), 
+            grads["params"], 
             max_mean_grad=1e9, 
             min_mean_grad=1e-9
         )
