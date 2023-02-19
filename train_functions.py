d06e5ae7fba9372c0c6b2a6adb548c87b9139839
ChristianOrr <orrchristian@gmail.com>
2023-02-19 14:17:42 +0200

    minor updates

--- train_functions.py
+++ train_functions.py
@@ -23,10 +23,6 @@ def train(args, model, dtype):
     dataset = load_dataset(args.dataset_name)
     train_dataset, val_dataset  = dataset["train"], dataset["validation"]
 
-    # Number of segmentation classes
-    num_classes = args.num_classes
-    # Image height and width
-    height, width = args.height, args.width
     # Location for saving and loading checkpoints
     checkpoint_dir = args.weights_path
     os.makedirs(checkpoint_dir, exist_ok=True)
@@ -34,7 +30,7 @@ def train(args, model, dtype):
     # Display the model details
     dummy_x = jnp.array(train_dataset[0]["image"], dtype=dtype)
     # Downsample the image
-    dummy_x = jax.image.resize(dummy_x, shape=(height, width, 3), method="bilinear")
+    dummy_x = jax.image.resize(dummy_x, shape=(args.height, args.width, 3), method="bilinear")
     dummy_x = dummy_x[None, ...]
     rng_key = jax.random.PRNGKey(args.rng)
 
@@ -44,23 +40,11 @@ def train(args, model, dtype):
         augmax.RandomBrightness(range=(-0.6, 0.6), p=1.0),
     ))
 
-
-    # Number of training epochs
-    epochs = args.num_epochs
-    # Display training loss frequency
-    print_freq = args.print_freq
-    # Validation frequency
-    val_epochs = args.val_epochs
-    val_batches = args.val_batches
-    # Checkpointing frequency
-    save_steps = args.save_freq
-    batch_size = args.batch_size
-    # Background class
-    ignore_label = args.ignore_label
+    base_lr = args.lr * args.batch_size / 256.0
     # Exponential decay learning rate schedule defined by:
     # learning_rate * decay_rate ^ (global_step / decay_steps)
     schedule = optax.exponential_decay(
-        init_value=args.lr,
+        init_value=base_lr,
         transition_steps=args.decay_steps,
         decay_rate=args.decay,
         transition_begin=args.decay_start,
@@ -85,30 +69,41 @@ def train(args, model, dtype):
             target=state, 
             orbax_checkpointer=orbax_checkpointer
         )
+    # epoch_offset > 0 if restarting from checkpoint
+    epoch_offset = int(state.step)
 
     train_data_generator = create_infinite_generator(train_dataset)
     val_data_generator = create_infinite_generator(val_dataset)
 
     losses = []
 
-    for epoch in range(epochs + 1):
-        if epoch % print_freq == 0: print(f"Epoch: {epoch}")
+    for epoch in range(epoch_offset, args.num_epochs + 1):
+        if epoch % args.print_freq == 0: print(f"Epoch: {epoch}")
         # Evaluate the models performance
-        if epoch % val_epochs == 0:
+        if epoch % args.val_epochs == 0:
             eval_batch_metrics = []
-            for _ in range(val_batches):
+            for _ in range(args.val_batches):
                 val_inputs = []
                 # Loop until a valid batch is found
                 while len(val_inputs) == 0:
-                    val_inputs, val_targets = prep_data_batch(val_data_generator, batch_size, height, width, dtype=jnp.float32)
+                    val_inputs, val_targets = prep_data_batch(val_data_generator, args.batch_size, args.height, args.width, dtype=jnp.float32)
                 # Perform augmentation
                 rng_key, subkey = jax.random.split(rng_key)
                 if augment: val_inputs = augment_images(subkey, val_inputs)
                 # Get loss and preds
                 variables = freeze({"params": state.params})
-                val_loss, val_logits = loss_fn(variables, state, val_inputs, val_targets, num_classes, args.loss_method, ignore_label)
+                val_loss, val_logits = loss_fn(
+                    variables, 
+                    state, 
+                    val_inputs, 
+                    val_targets, 
+                    args.num_classes, 
+                    args.loss_method, 
+                    args.ignore_label,
+                    args.reg_coeff
+                    )
 
-                eval_metrics = compute_metrics(val_logits, val_targets, num_classes, ignore_label)
+                eval_metrics = compute_metrics(val_logits, val_targets, args.num_classes, args.ignore_label)
                 eval_metrics["loss"] = val_loss
                 eval_batch_metrics.append(eval_metrics)
 
@@ -119,19 +114,28 @@ def train(args, model, dtype):
         inputs = []
         # Loop until a valid batch is found
         while len(inputs) == 0:
-            inputs, targets = prep_data_batch(train_data_generator, batch_size, height, width, dtype=jnp.float32)
+            inputs, targets = prep_data_batch(train_data_generator, args.batch_size, args.height, args.width, dtype=jnp.float32)
         # Perform augmentation
         rng_key, subkey = jax.random.split(rng_key)
         if augment: inputs = augment_images(subkey, inputs)
         # Perform backpropagation 
         variables = freeze({"params": state.params})
         grad_fn = jax.value_and_grad(loss_fn, argnums=0, has_aux=True)
-        (loss, logits), grads = grad_fn(variables, state, inputs, targets, num_classes, args.loss_method, ignore_label)
+        (loss, logits), grads = grad_fn(
+            variables, 
+            state, 
+            inputs, 
+            targets, 
+            args.num_classes, 
+            args.loss_method, 
+            args.ignore_label,
+            args.reg_coeff
+            )
         state = state.apply_gradients(grads=grads["params"])
 
         losses.append(loss)
-        last_mean_loss = np.array(losses[-print_freq:]).mean()
-        if epoch % print_freq == 0: print(f"\tLoss: {last_mean_loss :.4f} \tLearning Rate: {schedule(epoch) :.8f}")
+        last_mean_loss = np.array(losses[-args.print_freq:]).mean()
+        if epoch % args.print_freq == 0: print(f"\tLoss: {last_mean_loss :.4f} \tLearning Rate: {schedule(epoch) :.8f}")
 
         has_vanished,  has_exploded, mean_grads = grads_vanished_or_exploded(
             grads["params"], 
@@ -148,7 +152,7 @@ def train(args, model, dtype):
             print("Gradients are nan, exiting training run now...")
             break
 
-        if epoch % save_steps == 0 and epoch != 0:
+        if epoch % args.save_steps == 0 and epoch != 0:
             checkpoints.save_checkpoint(
                 ckpt_dir=checkpoint_dir, 
                 target=state, 
